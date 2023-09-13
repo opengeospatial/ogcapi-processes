2f2134c1f8edf08fea9e7a50966323428e3539e1
Jerome St-Louis <jerome@ecere.com>
2023-09-13 12:44:14 -0400

    openapi: Updated bundled JSON API definition

---
+++ openapi/update.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Refresh pProcessListDeploy.yaml content
+rm paths/processes-dru/pProcessListDeploy.yaml ; \
+for i in processes-core/pProcessList.yaml \
+    processes-dru/pDeploy.yaml ; \
+do \
+   cat paths/$i >> paths/processes-dru/pProcessListDeploy.yaml;
+done
+
+# Refresh pProcessDescriptionReplaceUndeploy.yaml content
+rm paths/processes-dru/pProcessDescriptionReplaceUndeploy.yaml; \
+for i in processes-core/pProcessDescription.yaml \
+    processes-dru/pReplace.yaml \
+    processes-dru/pUndeploy.yaml ; \
+do \
+   cat paths/$i >> paths/processes-dru/pProcessDescriptionReplaceUndeploy.yaml ;\
+done
+
+# Bundle with swagger-cli
+swagger-cli bundle ogcapi-processes.yaml -o ogcapi-processes.bundled.json
